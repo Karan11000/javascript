31557639e8d062eb234cd13305977a5bb25b1df1
Karan Dua <76610086+Karan11000@users.noreply.github.com>
2022-02-04 14:48:39 +0530

    Update codeQuality.js

--- codeQuality.js
+++ codeQuality.js
@@ -1,1 +1,16 @@
 // level {1/2/3}
+// Level 2
+
+async function getUsers() {
+    const userProfile = {};
+    let doc = await Promise.all(getUser(), getProfile(), getPosts()).then((res)=>{
+        userProfile.user = res[0];
+        userProfile.Profile = res[1];
+        userProfile.p = res[2];
+    }).catch(err=>{
+        console.log(err);
+        throw new Error(err);
+    })
+    
+    return userProfile;
+  }
